7594259fd68218200fab1d3dd293df4191dfc64a
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-05-23 10:40:25 -0300

    Coliders

---
+++ Assets/Script/collisionDetector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colisionDetector : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("contacto");
+        if(collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

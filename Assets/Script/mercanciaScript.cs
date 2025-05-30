ce9334445536c27124a00a1a52e87a5f540b53c5
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-05-30 10:30:54 -0300

    Money Managar arreglo

--- Assets/Script/mercanciaScript.cs
+++ Assets/Script/mercanciaScript.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class mercanciaScript : MonoBehaviour
 {
-    public float cost;
+    public float cost = 5;
+    public moneyManager moneyManager;
+
+     void Start()
+    {
+        moneyManager = FindObjectOfType<moneyManager>();
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            moneyManager.UpdateMoney(-cost);
             Destroy(gameObject);
         }
     }

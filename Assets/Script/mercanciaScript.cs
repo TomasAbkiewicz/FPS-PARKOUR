cdb1ff80d21f05b907e76f3d59adfb3814b0dca4
TomasAbkiewicz <166508961+TomasAbkiewicz@users.noreply.github.com>
2025-05-30 10:03:03 -0300

    Money Manager

---
+++ Assets/Script/mercanciaScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mercanciaScript : MonoBehaviour
+{
+    public float cost;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/moneyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            // impedir compra
+            Debug.Log("dinero insuficiente");
+        }
+        else
+        {
+            money += amount;
+        }
+
+
+    }
+}
